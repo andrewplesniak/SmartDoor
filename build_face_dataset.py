b530a3bbbeca8c850e18f3605235c56834fb6962
Andrew <andrewplesniak@gmial.com>
2019-02-07 15:36:55 -0500

    added support for CNN based facial detection and enabled it as the default

--- build_face_dataset.py
+++ build_face_dataset.py
@@ -1,8 +1,9 @@
 # USAGE
-# python build_face_dataset.py --cascade haarcascade_frontalface_default.xml --output dataset/adrian
+# python build_face_dataset.py --dectmethod (Example input: haar or cnn(default)) --output (Example input: dataset/andrew)
 
 # import the necessary packages
 from imutils.video import VideoStream
+import numpy as np
 import argparse
 import imutils
 import time
@@ -11,14 +12,22 @@ import os
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-c", "--cascade", required=True,
-	help = "path to where the face cascade resides")
+ap.add_argument("-m", "--dectmethod", default='cnn',
+	help = "facial dection method - cnn or haar")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+	help="minimum probability to filter weak detections")
 ap.add_argument("-o", "--output", required=True,
 	help="path to output directory")
 args = vars(ap.parse_args())
 
 # load OpenCV's Haar cascade for face detection from disk
-detector = cv2.CascadeClassifier(args["cascade"])
+#detector = cv2.CascadeClassifier(args["cascade"])
+if args["dectmethod"] == 'haar':
+	detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+else:
+	print("[INFO] loading model...")
+	net = cv2.dnn.readNetFromCaffe('deploy.prototxt.txt', 'res10_300x300_ssd_iter_140000.caffemodel')
+
 
 # initialize the video stream, allow the camera sensor to warm up,
 # and initialize the total number of example faces written to disk
@@ -38,14 +47,51 @@ while True:
 	orig = frame.copy()
 	frame = imutils.resize(frame, width=400)
 
-	# detect faces in the grayscale frame
-	rects = detector.detectMultiScale(
-		cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), scaleFactor=1.1, 
-		minNeighbors=5, minSize=(30, 30))
+	if args["dectmethod"] == 'haar': #haar detection
+		# detect faces in the grayscale frame
+		rects = detector.detectMultiScale(
+			cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), scaleFactor=1.1, 
+			minNeighbors=5, minSize=(30, 30))
 
-	# loop over the face detections and draw them on the frame
-	for (x, y, w, h) in rects:
-		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+		# loop over the face detections and draw them on the frame
+		for (x, y, w, h) in rects:
+			cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+	else: #CNN detection
+		# grab the frame dimensions and convert it to a blob
+		(h, w) = frame.shape[:2]
+		blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
+			(300, 300), (104.0, 177.0, 123.0))
+	 
+		# pass the blob through the network and obtain the detections and
+		# predictions
+		net.setInput(blob)
+		detections = net.forward()
+
+		# loop over the detections
+		for i in range(0, detections.shape[2]):
+			# extract the confidence (i.e., probability) associated with the
+			# prediction
+			confidence = detections[0, 0, i, 2]
+
+			# filter out weak detections by ensuring the `confidence` is
+			# greater than the minimum confidence
+			if confidence < args["confidence"]:
+				continue
+
+			# compute the (x, y)-coordinates of the bounding box for the
+			# object
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY) = box.astype("int")
+	 
+			# draw the bounding box of the face along with the associated
+			# probability
+			text = "{:.2f}%".format(confidence * 100)
+			y = startY - 10 if startY - 10 > 10 else startY + 10
+			cv2.rectangle(frame, (startX, startY), (endX, endY),
+				(0, 0, 255), 2)
+			cv2.putText(frame, text, (startX, y),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+
 
 	# show the output frame
 	cv2.imshow("Frame", frame)
