298de83d7c48f05e0af61ca8a81203acbd3735f1
Andrew <andrewplesniak@gmial.com>
2019-03-05 19:18:53 -0500

    left and right arrow keys now control locked vs unlocked'

--- servo.py
+++ servo.py
@@ -12,9 +12,8 @@ def main(stdscr):
     global bus
     bus = smbus.SMBus(1)
     addr = 0x40
-    minvalue1 = 682 #values were found via experimentation based on rotational limits
-    maxvalue1 = 3409 #roughly equiv to 0.5ms to 2.5ms
-    stepsize = 10    #increase to increase speed
+    locked = 700 #values were found via experimentation based on what values cuase the door to unlock
+    unlocked = 1925 
 
     ##Setup and Config
     bus.write_byte_data(addr, 0, 0x20) #enable the chip
@@ -25,19 +24,19 @@ def main(stdscr):
     bus.write_byte_data(addr, 0, 0x20) #enable the chip
 
     ##Initalize
-    angle1 = int(round((minvalue1+maxvalue1)/2))
+    state = locked
     bus.write_byte_data(addr, 0x06, 0) #ch0 start time = 0s
     
     while True:
         x=stdscr.getch()
-        if x == curses.KEY_LEFT and (angle1 + stepsize) < maxvalue1:
-            angle1 = angle1 + stepsize
-            print(angle1)
-        elif x == curses.KEY_RIGHT and (angle1 - stepsize) > minvalue1:
-            angle1 = angle1 - stepsize
-            print(angle1)
+        if x == curses.KEY_LEFT:
+            state = locked
+            print("Locked")
+        elif x == curses.KEY_RIGHT:
+            state = unlocked
+            print("Unlocked")
 
-        bus.write_word_data(addr, 0x08, angle1) #writes to the stop address of Ch0   
+        bus.write_word_data(addr, 0x08, state) #writes to the stop address of Ch0   
 
 try:
     curses.wrapper(main)
