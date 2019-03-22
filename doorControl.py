8d365a87f649b4a79a47905b4d8400a5d5d9d5e0
andrewplesniak <andrewplesniak@gmail.com>
2019-03-22 12:22:40 -0400

    fixed class syntax errors

--- doorControl.py
+++ doorControl.py
@@ -5,35 +5,35 @@
 import smbus
 import time
 
-class door ():
-	def __init__():
+class door:
+	def __init__(self):
 		self.bus = smbus.SMBus(1)
 		self.addr = 0x40
 		self.locked = 700 #values were found via experimentation based on what values cuase the door to unlock
 		self.unlocked = 1925 
 
 		##Setup and Config
-		bus.write_byte_data(self.addr, 0, 0x20) #enable the chip
+		self.bus.write_byte_data(self.addr, 0, 0x20) #enable the chip
 		time.sleep(0.25)
-		bus.write_byte_data(self.addr, 0, 0x10) #enable Prescale change as noted in the datasheet
+		self.bus.write_byte_data(self.addr, 0, 0x10) #enable Prescale change as noted in the datasheet
 		time.sleep(0.25) #delay for reset
-		bus.write_byte_data(self.addr, 0xfe, 0x11) #changes the Prescale register value for 333Hz, using the equation in the datasheet
-		bus.write_byte_data(self.addr, 0, 0x20) #enable the chip
+		self.bus.write_byte_data(self.addr, 0xfe, 0x11) #changes the Prescale register value for 333Hz, using the equation in the datasheet
+		self.bus.write_byte_data(self.addr, 0, 0x20) #enable the chip
 
 		##Initalize
 		self.state = self.locked
-		bus.write_byte_data(self.addr, 0x06, 0) #ch0 start time = 0s
-		bus.write_word_data(self.addr, 0x08, self.state) #writes to the stop address of Ch0   
+		self.bus.write_byte_data(self.addr, 0x06, 0) #ch0 start time = 0s
+		self.bus.write_word_data(self.addr, 0x08, self.state) #writes to the stop address of Ch0   
 
 	def unlock(self):
 		self.state = self.unlocked
-		bus.write_byte_data(self.addr, 0x06, 0) #ch0 start time = 0s
-		bus.write_word_data(self.addr, 0x08, self.state) #writes to the stop address of Ch0   
+		self.bus.write_byte_data(self.addr, 0x06, 0) #ch0 start time = 0s
+		self.bus.write_word_data(self.addr, 0x08, self.state) #writes to the stop address of Ch0   
 
 	def lock(self):
 		self.state = self.locked
-		bus.write_byte_data(self.addr, 0x06, 0) #ch0 start time = 0s
-		bus.write_word_data(self.addr, 0x08, self.state) #writes to the stop address of Ch0   
+		self.bus.write_byte_data(self.addr, 0x06, 0) #ch0 start time = 0s
+		self.bus.write_word_data(self.addr, 0x08, self.state) #writes to the stop address of Ch0   
 
 
 
