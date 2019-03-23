30a7be12f3ae6c9c4500c8bafbf4f627f0c01a0a
Andrew <andrewplesniak@gmial.com>
2019-03-23 19:58:51 -0400

    server code now actually unlocks and locks the door

--- server.py
+++ server.py
@@ -6,17 +6,31 @@ from flask import Flask, render_template, Response
 import RPi
 import RPi.GPIO as GPIO
 
-import led
+import doorControl
 
 app = Flask(__name__)
 logger = logging.Logger('SmartDoor')
+door = doorControl.door()
 
 @app.route("/")
 def hello_world():
     return "Hello world!"
-@app.route("/dooropen")
-def dooropen():
-    led.openLed()
-    return 'success'
+
+@app.route("/unlock")
+def doorunlock():
+    door.unlock()
+    return 'Door unlocked'
+
+@app.route("/lock")
+def doorlock():
+    door.lock()
+    return 'Door locked'
+
+@app.route("/shutdown")
+def doorshutdown():
+    door.unlock()
+    door.shutdown()
+    return 'Door shutdown'
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host = '192.168.1.234')
